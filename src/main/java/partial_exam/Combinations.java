829c5ccd6eaff8915eb56bf694b81225834ac88b
frank Jonislla <frank.che88@gmail.com>
2018-06-02 13:39:17 -0500

    pregunta 3

--- src/main/java/partial_exam/Combinations.java
+++ src/main/java/partial_exam/Combinations.java
@@ -1,11 +1,32 @@
 package partial_exam;
 
 public class Combinations {
+	
+	
+	/*
+	 * 
+	 * Una escalera tiene N pasos, una persona puede subir de 1, 2 o 3 pasos a la vez, 
+	 * se requirió retornar el número de combinaciones posibles en la que se puede subir 
+	 * la escalera para lo cual se escribió el siguiente algoritmo:
+	 * 
+	 */
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		Combinations combinations = new Combinations();
-		int n = 5;
-		System.out.println("# Ladder's steps: " + n);
-		System.out.println("# Step combinations: " + combinations.countCombinations(n, 0));
+		
+		
+		
+		
+		for(int n=0; n<25; n++) {
+			System.out.println(n + "\t" + combinations.countCombinationsDinamica(n, 0));
+			
+		}
+		
+		
 	}
 
 	public long countCombinations(int n, int level) {
@@ -21,4 +42,36 @@ public class Combinations {
 		return countCombinations(n - 1, level + 1) + countCombinations(n - 2, level + 1)
 				+ countCombinations(n - 3, level + 1);
 	}
+	
+	
+	public long countCombinationsDinamica(int n, int level) {
+		
+		
+		if (n == 0 ) {
+			return 0;
+		}
+		
+
+		if (n == 1 ) {
+			return 1;
+		}
+		
+		if (n == 2) {
+			return 2;
+		}
+		long vector[] = new long[n+1] ;
+		vector[0] = 1;
+		vector[1] = 1;
+		vector[2] = 2;
+		
+		for(int k=3; k < n + 1; k++) {
+			vector[k] = vector[k-1] + vector[k-2] + vector[k-3];
+		}
+		
+		return vector[n];
+		
+	}
+
+
+
 }
